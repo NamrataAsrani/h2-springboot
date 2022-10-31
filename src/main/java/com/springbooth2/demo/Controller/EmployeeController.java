0600238f2f424b1b7c03ff07d1497f18ed247bae
Asrani <namrata.asrani@dxc.com>
2022-10-31 15:06:57 +0530

    controller commit

--- src/main/java/com/springbooth2/demo/Controller/EmployeeController.java
+++ src/main/java/com/springbooth2/demo/Controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +24,17 @@ public class EmployeeController {
 	
 	//search, update and delete operations
 	//get all 
+	
+	@RequestMapping("/Hello")
+	public String display() {
+		//repository.toString(emp);
+		return "Welcome Springboot";
+	}
+	
 	@RequestMapping(value="/employees",method =RequestMethod.GET)
 	List<Employee> getEmployee() {
 		return repository.findAll();
+		
 		
 		
 	}
